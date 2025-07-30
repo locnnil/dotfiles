06fce02d0faca5ec3e329584fb06de61ab09c1d4
Lincoln Wallace <locnnil0@gmail.com>
2025-07-30 17:45:55 -0300

    chore: update lazyvim plugins
    Signed-off-by: Lincoln Wallace <locnnil0@gmail.com>

---
+++ neovim/.config/nvim/lua/plugins/lint.lua
@@ -0,0 +1,14 @@
+-- This doesn't work
+
+return {
+  {
+    "mfussenegger/nvim-lint",
+    opts = {
+      linters = {
+        markdownlint = {
+          args = { "--disable", "MD013", "--" },
+        },
+      },
+    },
+  },
+}
